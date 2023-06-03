f33674f28d5de034994fc6e6fbc5952ac53b299c
my_username <my_email_address>
2023-06-03 16:40:53 +0300

    committtcomittttmcomvr

--- stats.js
+++ stats.js
@@ -5,3 +5,31 @@ let mountains = [
     { name: "", height: 1592, place: "" },
     { name: "", height: 1738, place: "" }
   ];
+  const workoutLog = [
+    { date: '2023-05-30', exercise: 'Push-ups', sets: 3, reps: 10 },
+    { date: '2023-05-31', exercise: 'Plank', sets: 3, reps: 1- 2 },
+    { date: '2023-06-01', exercise: 'Squats', sets: 3, reps: 10 },
+   
+  ];
+  
+  
+  let totalSets = 0;
+  let totalReps = 0;
+  
+  for (const entry of workoutLog) {
+    totalSets += entry.sets;
+    totalReps += entry.reps;
+  }
+  
+ 
+  const averageSets = totalSets / workoutLog.length;
+  const averageReps = totalReps / workoutLog.length;
+  
+  
+  console.log('Date\t\tExercise\tSets\tReps');
+  for (const entry of workoutLog) {
+    console.log(`${entry.date}\t${entry.exercise}\t${entry.sets}\t${entry.reps}`);
+  }
+  console.log('---------------------------------');
+  console.log(`Total:\t\t\t${totalSets}\t${totalReps}`);
+  console.log(`Average:\t\t${averageSets.toFixed(2)}\t${averageReps.toFixed(2)}`);
