67bddcad44afce3a3a791067fd05013c6ef187bb
okilani <my_email_addressokilani.26@acsamman.edu.jo>
2023-06-03 16:28:14 +0300

    commit73

---
+++ stats.js
@@ -0,0 +1,7 @@
+let mountains = [
+    { name: "Day:", height: 1658, place: "Workout" },
+    { name: "", height: 1654, place: "" },
+    { name: "", height: 1520, place: "" },
+    { name: "", height: 1592, place: "" },
+    { name: "", height: 1738, place: "" }
+  ];
